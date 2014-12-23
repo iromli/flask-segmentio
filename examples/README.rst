4a956e2a71ae64f86ad72d838b20758e2a8ebb20
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-12-24 05:17:24 +0700

    add example

---
+++ examples/README.rst
@@ -0,0 +1,17 @@
+Example
+~~~~~~~
+
+Install and run:
+
+.. sourcecode:: sh
+
+    pip install flask
+    SEGMENTIO_WRITE_KEY=your_write_key python app.py
+
+Test:
+
+.. sourcecode:: sh
+
+    curl http://localhost:5000/
+
+Afterwards, go to your Segment dashboard and see the result in debugger tab.
---
+++ examples/app.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+
+from flask import Flask
+from flask_segmentio.client import SegmentIO
+
+SEGMENTIO_WRITE_KEY = os.environ.get("SEGMENTIO_WRITE_KEY", "")
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+analytics = SegmentIO()
+analytics.init_app(app)
+
+
+@app.route("/")
+def index():
+    analytics.track(
+        "johndoe",
+        "Posted a blog",
+        {
+            "title": "hello world",
+            "content": "this is a test",
+        })
+    return "<h1>SENT</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
